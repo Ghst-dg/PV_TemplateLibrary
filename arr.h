acbc7128042b72c7dfe6c86e0d8a457b7016b794
ghstdg <gh0stm0nkey@protonmail.ch>
2022-01-30 11:05:09 +0530

    fixed PV::Array

--- arr.h
+++ arr.h
@@ -1,25 +1,62 @@
 #pragma once
+#define ull unsigned long long int
 
 namespace PV
 {
-	template <typename T, int S>
+	template <typename T,ull S>
 	class Array
 	{
 	public:
-		int Size() { return S; }
-		T& operator[](int index) { return array[index]; }
-		T Front() { return array[0]; }
-		T Back() { return array[S - 1]; }
-		int Search(T value)
-		{
-			for (int i = 0; i < S; i++)
-				if (value == array[i])
-					return i;
+		int Size();
+		bool Empty();
 
-			return -1;
-		}
+		T& operator[](int index);
+		T& At(int index);
+		T Front();
+		T Back();
 
 	private:
 		T array[S] = { 0 };
 	};
+
+	template<typename T,ull S>
+	inline int Array<T, S>::Size()
+	{
+		return S;
+	}
+
+	template<typename T, ull S>
+	inline bool Array<T, S>::Empty()
+	{
+		if (S == 0)
+			return true;
+
+		return false;
+	}
+
+	template<typename T, ull S>
+	inline T& Array<T, S>::operator[](int index)
+	{
+		return array[index];
+	}
+
+	template<typename T, ull S>
+	inline T& Array<T, S>::At(int index)
+	{
+		return array[index];
+	}
+
+	template<typename T, ull S>
+	inline T Array<T, S>::Front()
+	{
+		return array[0];
+	}
+
+	template<typename T, ull S>
+	inline T Array<T, S>::Back()
+	{
+		return array[S - 1];
+	}
+	
+
 }
