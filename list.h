fa382eaa31062e7a47d0eb623ecdc9791d7eb3be
Priyanshu Verma <gh0stm0nkey@protonmail.ch>
2022-02-02 19:42:14 +0530

    added list

--- list.h
+++ list.h
@@ -1,56 +1,207 @@
 #pragma once
-#define autovariable template<typename T>
+#define ull unsigned long long int
 
 namespace PV
 {
-	autovariable
-	class Element
+	template <typename T>
+	class List
 	{
 	public:
-		Element* next;
-		Element* previous;
-		T data = 0;
+		List(T value = 0);
+		~List();
+
+		T& Front();
+		T& Back();
+
+		T PushBack(T value);
+		T PushFront(T value);
+		T PopBack();
+		T PopFront();
+		T Insert(T value, ull index);
+		T Erase(ull index);
+		void Swap(ull index1, ull index2);
+
+		void Reverse();
+
+
+	private:
+		class Node
+		{
+		public:
+			Node* next;
+			T data;
+		};
+		Node* node = new Node();
 	};
 
-	autovariable
-	Element<T>* l_CreateNode();
+	template<typename T>
+	inline List<T>::List(T value)
+	{
+		node->next = nullptr;
+		node->data = value;
+	}
 
-	autovariable
-		Element<T>* l_CreateNode(T value);
+	template<typename T>
+	inline List<T>::~List()
+	{
+		while (node != nullptr)
+		{
+			Node* temp = node;
+			node = node->next;
+			delete temp;
+		}
+	}
 
-	autovariable
-		void l_PrintList(Element<T>* node);
+	template<typename T>
+	inline T& List<T>::Front()
+	{
+		return node->data;
+	}
 
-	autovariable
-		void l_PrintList(Element<T>* node, int index);
+	template<typename T>
+	inline T& List<T>::Back()
+	{
+		Node* temp = node;
 
-	autovariable
-		void l_BackPrintList(Element<T>* node);
+		while (temp->next != nullptr)
+		{
+			temp = temp->next;
+		}
 
-	autovariable
-		void l_PushNode(Element<T>** head, int value);
+		return temp->data;
+	}
 
-	autovariable
-		void l_AppendNode(Element<T>** head, int value);
+	template<typename T>
+	inline T List<T>::PushBack(T value)
+	{
+		Node* head = node;
+		Node* temp = new Node();
+		temp->data = value;
+		temp->next = nullptr;
 
-	autovariable
-		void l_InsertAt(Element<T>** head, int value, int index);
+		while(head->next != nullptr)
+		{
+			head = head->next;
+		}
 
-	autovariable
-		void l_InsertBefore(Element<T>** head, int value, int index);
+		head->next = temp;
 
-	autovariable
-		void l_InsertAfter(Element<T>** head, int value, int index);
+		return value;
+	}
 
-	autovariable
-		void l_DeleteNode(Element<T>** head, int index);
+	template<typename T>
+	inline T List<T>::PushFront(T value)
+	{
+		Node* temp = new Node();
+		temp->data = value;
+		temp->next = node;
+		node = temp;
+		return value;
+	}
 
-	autovariable
-		void l_ReverseList(Element<T>** head);
+	template<typename T>
+	inline T List<T>::PopBack()
+	{
+		Node* temp = node;
+		while (temp->next->next != nullptr)
+			temp = temp->next;
 
-	autovariable
-		int l_FindNode(Element<T>* node, int value);
+		T value = temp->next->data;
+		delete temp->next;
+		temp->next = nullptr;
+		return value;
+	}
 
-	autovariable
-		void l_JoinList(Element<T>* head1, Element<T>* head2);
+	template<typename T>
+	inline T List<T>::PopFront()
+	{
+		Node* temp = node;
+		node = node->next;
+		T value = temp->data;
+		delete temp;
+		return value;
+	}
+
+	template<typename T>
+	inline T List<T>::Insert(T value, ull index)
+	{
+		if (index == 0)
+			PushFront(value);
+
+		else
+		{
+			Node* temp = node;
+			while (temp->next != nullptr && index > 1)
+			{
+				temp = temp->next;
+				--index;
+			}
+			Node* var = new Node();
+			var->next = temp->next;
+			var->data = value;
+			temp->next = var;
+		}
+
+		return value;
+	}
+	template<typename T>
+	inline T List<T>::Erase(ull index)
+	{
+		if (index == 0)
+			return PopFront();
+
+		Node* temp = node;
+		while (temp->next != nullptr && index > 1)
+		{
+			temp = temp->next;
+			--index;
+		}
+
+		Node* var = temp->next;
+		temp->next = var->next;
+		T value = var->data;
+		delete var;
+		return value;
+	}
+
+	template<typename T>
+	inline void List<T>::Swap(ull index1, ull index2)
+	{
+		Node* temp1 = node;
+		Node* temp2 = node;
+
+		while (index1 > 0)
+		{
+			temp1 = temp1->next;
+			--index1;
+		}
+
+		while (index2 > 0)
+		{
+			temp2 = temp2->next;
+			--index2;
+		}
+
+		temp1->data = temp1->data + temp2->data;
+		temp2->data = temp1->data - temp2->data;
+		temp1->data = temp1->data - temp2->data;
+	}
+
+	template<typename T>
+	inline void List<T>::Reverse()
+	{
+		Node* prev = nullptr;
+		Node* current = node;
+		Node* link = new Node();
+
+		while (current != nullptr)
+		{
+			link = current->next;
+			current->next = prev;
+			prev = current;
+			current = link;
+		}
+
+		node = prev;
+	}
 }
